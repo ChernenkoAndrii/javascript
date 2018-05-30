54fbd958a6ed2e755d9d3f9a5d72db09e91a6959
Andrii <pobedon1@gmail.com>
2018-05-30 15:55:36 +0000

    initial

---
+++ js/index.js
@@ -0,0 +1,30 @@
+///const userAge = parseInt(prompt('Enter your age!'));
+//const nextAge = userAge + 1;
+//const userName = prompt('Enter your name!');
+//const greet = 'Hello, next year u will be ' + nextAge + ' years old';
+
+//if(nextAge<20){
+   // alert('child');} else if    (nextAge<80){
+    //    alert('adult');}else{
+      //      alert('old');
+      //  }
+       function sum(a, b){
+           const result= a+b;
+            return result;
+            
+        }
+        let sumOfNums = sum (10, 20);
+       alert(sumOfNums);
+        sumOfNums = sum (20, 30);
+      alert(sumOfNums);
+        function sum3(a, b, c){
+            
+            return a + b + c;
+        }
+        let sumOfThree= sum3(16, 33, 12);
+        alert(sumOfThree);
+
+//const message = 'Vitajemo na nashomu saiti';
+//const greetings = 'Hello' + '!';
+//alert(greetings);
+//alert(message);

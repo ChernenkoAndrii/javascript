e07311ce0fce296cb7c7c2059acf07c046b0261d
Andrii <pobedon1@gmail.com>
2018-06-01 08:28:27 +0000

    initial

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@
     //    alert('adult');}else{
       //      alert('old');
       //  }
-       function sum(a, b){
+      /* function sum(a, b){
            const result= a+b;
             return result;
             
@@ -22,9 +22,38 @@
             return a + b + c;
         }
         let sumOfThree= sum3(16, 33, 12);
-        alert(sumOfThree);
+        alert(sumOfThree);*/
 
 //const message = 'Vitajemo na nashomu saiti';
 //const greetings = 'Hello' + '!';
 //alert(greetings);
 //alert(message);
+   
+    
+       let result;
+    let a=parseFloat(prompt('a'));
+    let b=parseFloat(prompt('b'));
+    let c=parseFloat(prompt('c'));
+    
+    function calcDiskr (a, b, c){
+        const result=Math.sqrt(b*b-4*a*c);
+        return result;
+    }
+    let d=calcDiskr(a, b, c);
+    function quadX(d){
+        
+        if(d>0){
+            const x1=(-b+d)/(2*a);
+            const x2=(-b-d)/(2*a);
+            
+            return result='x1 '+x1+' x2'+x2;
+        }
+        else if(d==0){
+            const x=(-b)/(2*a);
+            return result='x ' + x;
+        }
+        return result ="No answer";
+    }
+    let answer=quadX (d);
+    alert(answer);
+ 
